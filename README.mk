cb5f4b1e0d8e705f47e2a931fb508215847e08d4
Wojciech Szęszoł <keepitsimplesirius@gmail.com>
2019-09-18 18:45:50 +0200

    Add readme and screenshot.

---
+++ README.mk
@@ -0,0 +1,10 @@
+# Description
+A simple breakout clone written in Spring 2014
+
+# How to run
+pip3 install pygame
+python3 main.py
+
+# Issues
++ In python older than 3.7 requires workaround for issue Issue20891
++ Because of the drivers issues on Linux the sound effects have a big latency
